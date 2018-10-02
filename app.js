40ce3088c4bc6f95fae231e6565c9262e5b5a290
Shivamdubey809 <43633428+Shivamdubey809@users.noreply.github.com>
2018-10-02 16:57:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+ 
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    flash          = require("connect-flash"),
+    passport       = require("passport"),
+    LocalStrategy  = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground     = require("./models/campground"),
+    Comment        = require("./models/comment"),
+    User           = require("./models/user"),
+    seedDB         = require("./seeds");
+
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+
+mongoose.connect("mongodb://shivam:shivam1691@ds115963.mlab.com:15963/yelp_camp", {useNewUrlParser: true});
+//mongodb://shivam:shivam1691@ds115963.mlab.com:15963/yelp_camp
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+//seedDB(); //seed the database
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "Heyy Its a secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+
+app.use(indexRoutes);
+app.use(commentRoutes);
+app.use(campgroundRoutes);
+
+
+
+app.listen(process.env.PORT || 5000, process.env.IP, function(){
+
+      console.log("THE YELP CAMP HAS STARTED");
+
+});
